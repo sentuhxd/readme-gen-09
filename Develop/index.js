ce6a3fc33f9f60c46bdafba2716e73f7322e89ca
sentuh <142236429+sentuhxd@users.noreply.github.com>
2023-09-14 14:43:53 -0400

    added initializations

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,93 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your username ?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log("Please enter a username");
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email ?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log("Please enter your email");
+                    return false;
+                }
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What the title of your Project ?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }else {
+                    console.log("Please enter a Project Title");
+                    return false;
+                }
+            }
+        },
+
+    ]);
+};
+
+const writeToFile = data => {
+    fs.writeFile('README.md', data, err => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Your REAME was created')
+        }
+
+    })
+};
+
+const init = () => {
+    return inquirer.prompt(questions);
+}
+
+init()
+.then(userInput => {
+    generateMarkdown(userInput);
+})
+
+.then(data => {
+    return writeToFile(data);
+})
+.catch(err => {
+    console.log(err);
+})
+
+
+
+
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
-init();
+
